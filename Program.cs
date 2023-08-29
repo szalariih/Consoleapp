fc0d770dfc559149c2cfd047bb73cb8de0c64a16
Larissa <larissa.nascimento@varsolutions.com.br>
2023-08-29 20:11:37 -0300

    update

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    class program
+    {
+        public static string Mensagem = "Olá mundo";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mensagem);
+        }
+    }
+}
